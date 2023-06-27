0da0a74f2b0795b660d09068286b86fe24c52cee
ibrahimmohurlu <44712700+ibrahimmohurlu@users.noreply.github.com>
2023-06-27 21:18:21 +0300

    tailwind css added. Challenges listed.

---
+++ pages/challenges/age-calculator-app.tsx
@@ -0,0 +1,5 @@
+
+
+export default function AgeCalculatorApp(){
+    return <div>dlashdjsahdkjas</div>
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const challenges = [
+    {
+      name: "Age Calculator App",
+      challengeUrl: "/challenges/age-calculator-app",
+      frontendMentorUrl: "https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q"
+    }
+  ]
+
   return (
     <>
       <Head>
@@ -14,101 +22,35 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+      <div className="flex flex-col justify-evenly items-center min-h-screen p-24 bg-gray-800 text-gray-200">
+        <div className='text-4xl font-bold text-center'>
+          <p>Frontend Mentor Challenges</p>
+          <p className='text-2xl font-normal'>using Next.js and TailwindCSS</p>
+        </div>
+
+        <div className="grid grid-cols-4 gap-8 text-gray-800">
+          {challenges.map(challenge => {
+            return <Link
+              href={challenge.challengeUrl}
+              className="flex flex-col w-full gap-4 bg-gray-200 rounded px-4 py-5 border border-gray-800 items-center text-left"
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
+              <h2 className="font-bold text-2xl">
+                {challenge.name}
+              </h2>
+              <Link href={challenge.frontendMentorUrl} className="group overflow-hidden inline-flex gap-1 items-center text-blue-500 hover:underline hover:text-blue-600 text-lg">
+                Visit challenge
+                <span className='transition-transform duration-200 translate-y-6 -translate-x-6 group-hover:translate-y-0 group-hover:translate-x-0'>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
+                  </svg>
+                </span>
+              </Link>
+            </Link>
+          })}
         </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      </div>
     </>
   )
 }
